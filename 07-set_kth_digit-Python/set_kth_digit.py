ce718ca4b1203b2b4937434e2152a269365e5ca6
saideep9835 <tsaideep98@msitprogram.net>
2021-07-15 22:47:54 +0530

    M-2

--- 07-set_kth_digit-Python/set_kth_digit.py
+++ 07-set_kth_digit-Python/set_kth_digit.py
@@ -7,5 +7,24 @@
 
 
 def fun_set_kth_digit(n, k, d):
-		return 0
+	flag = False
+	if n<0:
+		n = -n
+		flag = True
+	count = 0
+	res = 0
+	while(n>0):
+		r = n%10
+		if count == k:
+			res += (d*(10**count))
+		else:
+			res = res+(r*(10**count))
+		count += 1
+		n = n//10
+	if count <= k:
+		res = res + (d*(10 ** count))
+	if flag:
+		return -res
+	else:
+		return res
 
