9b77e831f14acb68e7007db96fdeed5d07ce3e7a
saideep9835 <tsaideep98@msitprogram.net>
2021-07-16 15:38:50 +0530

    power

--- powersum.py
+++ powersum.py
@@ -12,8 +12,17 @@
 
 def powerSum(n, k):
     # Your code goes here...
+    b=0
+    if n>0 and k>0:
+        for i in range(1,n+1):
+            b = b+i**k
+        return b
+
     return 0
 
 # Write your own test cases here...
-
+print(powerSum(2,4))
+print(powerSum(3,4))
+print(powerSum(4,4))
+print(powerSum(5,4))
 print ("All test cases passed...")
