70e5115c9a1f73fad2e36619cd5d271a88e9de5f
saideep9835 <tsaideep98@msitprogram.net>
2021-07-18 11:53:16 +0530

    M-4

--- 03-applycaesarcipher-Python/applycaesarcipher.py
+++ 03-applycaesarcipher-Python/applycaesarcipher.py
@@ -9,9 +9,38 @@
 # assert(applyCaesarCipher("We Attack At Dawn", 1) == "Xf Buubdl Bu Ebxo")
 # assert(applyCaesarCipher("zodiac", -2) == "xmbgya")
 
-
+alpha= "abcdefghijklmnopqrstuvwxyz"
+be = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+ce = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 def fun_applycaesarcipher(msg, shift):
-	return ""
+	g=[]
+	o = ""
+	u = ""
+	for i in range(len(msg)):
+		if msg.islower():
+			ch = msg[i]
+			lo = alpha.find(ch)
+			new_lo=(lo+shift)%26
+			o += alpha[new_lo]
+			
+		elif msg.isupper():
+			ch = msg[i]
+			lo = be.find(ch)
+			new_lo=(lo+shift)%26
+			o += be[new_lo]
+	
+		else:
+		
+			ch = msg[i]
+			lo = ce.find(ch)
+			new_lo=(lo+shift)%52
+			o += ce[new_lo]
+			o=o.replace("a"," ")
+	return o
+	
+
+
+
 
 
 
