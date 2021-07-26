79aaab9da84f718231631198353cd7998fd51d4e
saideep9835 <tsaideep98@msitprogram.net>
2021-07-26 09:12:11 +0530

    d

--- 01-linkedlist-Python/linkedlist.py
+++ 01-linkedlist-Python/linkedlist.py
@@ -8,6 +8,9 @@ particular value.
 Then, use "Test Run" and "Submit" to run the test cases
 at the bottom."""
 
+from typing import Counter
+
+
 class Element(object):
     def __init__(self, value):
         self.value = value
@@ -19,14 +22,29 @@ class LinkedList(object):
         
     def append(self, new_element):
         # Your code goes here
-        pass
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
             
     def get_position(self, position):
         """Get an element from a particular position.
         Assume the first position is "1".
         Return "None" if position is not in the list."""
         # Your code goes here
-        pass
+        counter = 1
+        current = self.head
+        if position < 1:
+            return None
+        while current and counter <= position:
+            if counter == position:
+                return current
+            current = current.next
+            counter += 1
+        return None
     
     def insert(self, new_element, position):
         """Insert a new node at the given position.
@@ -34,7 +52,25 @@ class LinkedList(object):
         Inserting at position 3 means between
         the 2nd and 3rd elements."""
         # Your code goes here
-        pass
+        new = Element(new_element)
+        pointer = self.head
+  
+        counter = 1
+
+        while pointer.next is not None:
+
+            if counter == position:
+         
+                new.next = pointer.next
+            
+                pointer.next = new
+            break
+  
+        counter += 1
+        pointer = pointer.next
+  
+        return self.head
+    
     
     
     def delete(self, value):
